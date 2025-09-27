e296b959e703afdc1887a38bfefd249582705597
upethalaksiluni <upethalaksiluni@gmail.com>
2025-09-27 11:27:51 +0530

    All in memory repositories implemented

--- repositories/repositories.py
+++ repositories/repositories.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Dict
 
 from customer.customer import Customer
 from order.order import Order
@@ -57,3 +57,54 @@ class CustomerRepository(ABC):
     @abstractmethod
     def list_all_customers(self) -> List[Customer]:
         pass
+
+
+class InMemoryProductRepository(ProductRepository):
+
+    def __init__(self):
+        self.__products: Dict[str, Product] = {}
+
+    def add_product(self, product: Product) -> None:
+        self.__products[product.product_id] = product
+
+    def get_by_id(self, product_id) -> Product:
+        return self.__products.get(product_id)
+
+    def update(self, product: Product) -> None:
+        self.__products[product.product_id] = product
+
+    def list_all_products(self) -> List[Product]:
+        return list(self.__products.values())
+
+
+class InMemoryCustomerRepository(CustomerRepository):
+
+    def __init__(self):
+        self.__customers: Dict[str, Customer] = {}
+
+    def add_customer(self, customer: Customer) -> None:
+        self.__customers[customer.customer_id] = customer
+
+    def get_by_id(self, customer_id) -> Customer:
+        return self.__customers.get(customer_id)
+
+    def update(self, customer: Customer) -> None:
+        self.__customers[customer.customer_id] = customer
+
+    def list_all_customers(self) -> List[Customer]:
+        return list(self.__customers.values())
+
+
+class InMemoryOrderRepository(OrderRepository):
+
+    def __init__(self):
+        self.__orders: Dict[str, Order] = {}
+
+    def add(self, order: Order) -> None:
+        self.__orders[order.order_id] = order
+
+    def get_order_by_id(self, order_id) -> Order:
+        return self.__orders.get(order_id)
+
+    def list_all_orders(self) -> List[Order]:
+        return list(self.__orders.values())
